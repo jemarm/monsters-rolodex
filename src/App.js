c0792c25523d85f8e306bbd3c9ab3edb16ec5533
JayM647 <jemarmrrs@gmail.com>
2023-05-10 22:31:10 -0400

    updated with local code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+  }
 }
+
+componentDidMount() {
+  console.log('componentDidMount');
+  fetch('https://jsonplaceholder.typicode.com/users')
+   .then((response) => response.json())
+   .then((users) => 
+   this.setState(
+    () => { 
+      return { monsters: users };
+    },
+    () => {
+      console.log(this.state);
+    }
+   ));
+}
+
+
+  render() {
+    console.log('render');
+    return (
+      <div className="App">
+      <input 
+        classname="search-box" 
+        type="search"
+        placeholder="search monsters" 
+        onChange={(event) => console.log(event.target.value)} />
+        {this.state.monsters.map((monster) => {
+          return(
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+        )
+        })}
+      </div>
+    );
+  }
+}
+
+
 
 export default App;
